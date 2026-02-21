ea6d2012273534a06d36243b5c7596051a7cece5
dhaboav <100108392+dhaboav@users.noreply.github.com>
2026-02-21 16:18:49 +0700

    chore: add Docker configuration for frontend
    - Update img src for logo frontend to fix docker build

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,10 +1,11 @@
+import reactLogo from './assets/react.svg';
 import Button from './ui/Button';
 
 function Navbar() {
     return (
         <nav className="flex h-15 items-center justify-between border-b px-16">
             <div className="flex items-center gap-x-2">
-                <img src="src\assets\react.svg" className="animate-spin [animation-duration:10s]" alt='Logo'/>
+                <img src={reactLogo} className="animate-spin [animation-duration:10s]" alt="Logo" />
                 <h1 className="text-lg font-semibold">React Library</h1>
             </div>
             <Button content={'+ Add Book'} />
